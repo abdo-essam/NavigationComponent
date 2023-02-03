dc2e81ba8111059e02f90ecde24682b47f426dc1
abdo <abdo.e.mosa@gmail.com>
2023-02-03 19:49:36 +0200

    Navigation Component with Safe Args

---
+++ app/src/main/java/org/abdoessam/navigationcomponent/SecondFragment.kt
@@ -0,0 +1,30 @@
+package org.abdoessam.navigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+
+
+class SecondFragment : Fragment() {
+
+    val  args:SecondFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+        val muNumber = args.number
+        view.findViewById<TextView>(R.id.textView2).text = muNumber.toString()
+        view.findViewById<TextView>(R.id.textView2).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.navigateToFirstFragment)
+        }
+        return view
+    }
+}
